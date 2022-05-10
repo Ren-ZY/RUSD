314d6ebf84eba3eededec9ec33fc35f9b5a4597a
RenZY <39653953+Ren-ZY@users.noreply.github.com>
2022-05-10 11:34:03 +0800

    Update and rename rusd.rs to rustsoda.rs

--- src/bin/rustsoda.rs
+++ src/bin/rustsoda.rs
@@ -13,20 +13,20 @@ extern crate log;
 
 use std::env;
 
-use rusd::log::{setup_logging, Verbosity};
-use rusd::{compile_time_sysroot, main_entry, progress_info, RusdConfig, RUSD_DEFAULT_ARGS};
+use rustsoda::log::{setup_logging, Verbosity};
+use rustsoda::{compile_time_sysroot, main_entry, progress_info, RustSodaConfig, RUSTSODA_DEFAULT_ARGS};
 
-struct RusdCompilerCalls {
-    config: RusdConfig,
+struct RustSodaCompilerCalls{
+    config: RustSodaConfig,
 }
 
-impl RusdCompilerCalls {
-    fn new(config: RusdConfig) -> Self {
-        RusdCompilerCalls { config }
+impl RustSodaCompilerCalls{
+    fn new(config: RustSodaConfig) -> Self {
+        RustSodaCompilerCalls{ config }
     }
 }
 
-impl rustc_driver::Callbacks for RusdCompilerCalls {
+impl rustc_driver::Callbacks for RustSodaCompilerCalls{
     fn after_analysis<'tcx>(
         &mut self,
         compiler: &Compiler,
@@ -34,9 +34,9 @@ impl rustc_driver::Callbacks for RusdCompilerCalls {
     ) -> Compilation {
         compiler.session().abort_if_errors();
 
-        setup_logging(self.config.verbosity).expect("Rusd failed to initialize");
+        setup_logging(self.config.verbosity).expect("RustSoda failed to initialize");
 
-        progress_info!("Rusd started");
+        progress_info!("RustSoda started");
 
         
         queries.global_ctxt().unwrap().peek_mut().enter(|tcx| {
@@ -44,16 +44,16 @@ impl rustc_driver::Callbacks for RusdCompilerCalls {
         });
 
 
-        progress_info!("Rusd finished");
+        progress_info!("RustSoda finished");
 
         compiler.session().abort_if_errors();
         Compilation::Stop
     }
 }
 
-fn parse_config() -> (RusdConfig, Vec<String>) {
+fn parse_config() -> (RustSodaConfig, Vec<String>) {
     // collect arguments
-    let mut config = RusdConfig::default();
+    let mut config = RustSodaConfig::default();
 
     let mut rustc_args = vec![];
     for arg in std::env::args() {
@@ -89,17 +89,17 @@ fn run_compiler(
         }
     }
 
-    // Some options have different defaults in Rusd than in plain rustc; apply those by making
+    // Some options have different defaults in RustSoda than in plain rustc; apply those by making
     // them the first arguments after the binary name (but later arguments can overwrite them).
     args.splice(
         1..1,
-        rusd::RUSD_DEFAULT_ARGS.iter().map(ToString::to_string),
+        rustsoda::RUSTSODA_DEFAULT_ARGS.iter().map(ToString::to_string),
     );
 
     // Invoke compiler, and handle return code.
     let exit_code = rustc_driver::catch_with_exit_code(move || {
-        rustc_driver::RunCompiler::new(&args, callbacks).run()
-        //rustc_driver::run_compiler(&args, callbacks, None, None)
+        //rustc_driver::RunCompiler::new(&args, callbacks).run()
+        rustc_driver::run_compiler(&args, callbacks, None, None)
     });
 
     exit_code
@@ -129,10 +129,10 @@ fn main() {
         }
 
         // Finally, add the default flags all the way in the beginning, but after the binary name.
-        rustc_args.splice(1..1, RUSD_DEFAULT_ARGS.iter().map(ToString::to_string));
+        rustc_args.splice(1..1, RUSTSODA_DEFAULT_ARGS.iter().map(ToString::to_string));
 
         debug!("rustc arguments: {:?}", &rustc_args);
-        run_compiler(rustc_args, &mut RusdCompilerCalls::new(config))
+        run_compiler(rustc_args, &mut RustSodaCompilerCalls::new(config))
     };
 
     std::process::exit(exit_code)
